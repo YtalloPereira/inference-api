e2eab8ebe3e295d0c3185ee269fb4335a9b97926
YtalloAlves <ytollo.alves@academico.ifpb.edu.br>
2024-07-10 15:19:13 -0300

    :sparkles: feat: atualizando rota de inferencia  para usar o schema de resquest atualizado

--- api/routes/inference_route.py
+++ api/routes/inference_route.py
@@ -7,12 +7,24 @@ router = APIRouter()
 @router.post("/api/v1/inference", response_model=ResponseSchema)
 async def run_inference(request: RequestSchema):
     try:
-       
-        #implementar lógica de inferencia
-        result = 1  
+        input_data = [[
+            request.no_of_adults,
+            request.no_of_children,
+            request.required_car_parking_space,
+            request.lead_time,
+            request.arrival_year,
+            request.arrival_month,
+            request.arrival_date,
+            request.type_of_meal_plan,
+            request.room_type_reserved,
+            request.market_segment_type,
+            request.no_of_special_requests
+        ]]
+        #baixar model via s3 e usá-lo aqui
+        #prediction = model.predict(input_data)
+        ##result = 1  
 
-       
-        return ResponseSchema(result=result)
-    
+        #return ResponseSchema(result=int(prediction[0]))
+      
     except Exception as err:
         raise HTTPException(status_code=500, detail=f"Erro durante a inferência: {str(err)}")
