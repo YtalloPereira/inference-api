ee4a8a70d8d3f3d8626723e352557baa2d9147d2
YtalloAlves <ytollo.alves@academico.ifpb.edu.br>
2024-07-10 15:17:44 -0300

    :sparkles: feat: atualizando schema de request para ficar de acordo com os dados que serão usados na inferência

--- api/schemas/inference_request.py
+++ api/schemas/inference_request.py
@@ -1,6 +1,14 @@
 from pydantic import BaseModel
 
 class RequestSchema(BaseModel):
-    no_of_adults: int
-    no_of_children: int
-    type_of_meal_plan: str
+    no_of_adults :int
+    no_of_children :int
+    required_car_parking_space: int
+    lead_time :int
+    arrival_year :int
+    arrival_month :int
+    arrival_date :int
+    type_of_meal_plan :object
+    room_type_reserved :object
+    market_segment_type :object
+    no_of_special_requests :int
