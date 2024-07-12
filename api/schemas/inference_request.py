2b402102df90d281cd4ec44973adbc2062790d3c
YtalloAlves <ytollo.alves@academico.ifpb.edu.br>
2024-07-11 21:36:50 -0300

    :sparkles: feat: usando Literal no schema para acrescentar mais uma camada de validação

--- api/schemas/inference_request.py
+++ api/schemas/inference_request.py
@@ -1,4 +1,5 @@
 from pydantic import BaseModel
+from typing import Literal
 
 class RequestSchema(BaseModel):
     no_of_adults :int
@@ -8,7 +9,14 @@ class RequestSchema(BaseModel):
     arrival_year :int
     arrival_month :int
     arrival_date :int
-    type_of_meal_plan :str
-    room_type_reserved :str
-    market_segment_type :str
+    type_of_meal_plan: Literal["Meal Plan 1", "Meal Plan 2", "Meal Plan 3", "Not Selected"]
+    room_type_reserved: Literal[
+        "Room_Type 1", "Room_Type 2", "Room_Type 3", 
+        "Room_Type 4", "Room_Type 5", "Room_Type 6", 
+        "Room_Type 7"
+    ]
+    market_segment_type: Literal[
+        "Aviation", "Complementary", "Corporate", 
+        "Offline", "Online"
+    ]
     no_of_special_requests :int
